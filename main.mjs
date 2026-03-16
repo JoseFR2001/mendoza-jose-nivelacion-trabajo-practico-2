bb8ca1d162ed7556d36a76fa424b0c76f1292715
JoseFR2001 <jose.rafael.mendoza.2001@gmail.com>
2026-03-16 20:33:47 -0300

    El archivo .js terminado

--- main.mjs
+++ main.mjs
@@ -1,44 +1,61 @@
-import { createInterface } from 'readline/promises';
-import { stdin as input, stdout as output } from 'process';
+import { createInterface } from "readline/promises";
+import { stdin as input, stdout as output } from "process";
 
 const rl = createInterface({ input, output });
 
-const cantidadPersonas = await rl.question("¿Cuantás personas van a ser ingresados?");
+const personas = [];
+let sumaNotas = 0;
 
-console.log(cantidadPersonas)
+while (true) {
+  //Punto 1
+  const nombre = await rl.question(`Ingrese su nombre\n`);
 
-const personas = []
+  let edad = 0;
+  while (true) {
+    const edadRespuesta = await rl.question(`Ingrese su edad\n`);
+    const e = parseInt(edadRespuesta);
 
-let contador = 0
+    if (e > 0) {
+      edad = e;
+      break;
+    }
+    console.log("Edad inválida");
+  }
+  let nota = 0;
 
-while ( contador < cantidadPersonas ){
-    const nombre = await rl.question("Ingrese su nombre")
-    
-    const edadRespuesta = await rl.question("Ingrese su edad")
-    
-    const edad = parseInt(edadRespuesta)
+  while (true) {
+    const notaRespuesta = await rl.question(`Ingrese su nota\n`);
+    const n = parseFloat(notaRespuesta);
 
-    const notaRespuesta = await rl.question("Ingrese su nota")
-    
-    const nota = parseFloat(notaRespuesta)
+    if (n >= 0 && n <= 10) {
+      nota = n;
+      break; // Sale del bucle inmediatamente
+    }
+    console.log("Nota inválida");
+  }
+  sumaNotas += nota;
 
-    personas.push([nombre, edad, nota])
-    contador += 1
+  personas.push([nombre, edad, nota]);
+
+  const seguir = await rl.question(`Finalizar(Si/No)`);
+
+  if (seguir.toUpperCase() === "SI") {
+    break;
+  }
 }
 
-console.table(personas)
+//Punto 2
+const personasOrdenadas = [...personas].sort((a, b) => b[2] - a[2]);
 
-const personasOrdenadas = [... personas].sort((a, b) => b[2] - a[2])
-console.table(personasOrdenadas)
+const promedioNotas = sumaNotas / personas.length;
 
-const notasPersonas = personas.map(persona => persona[2])
-let sumaNotas = 0
+//Punto 3
+//Salida
+console.clear();
+console.log(`Personas:`);
+console.table(personas);
+console.log(`Personas ordenadas de mayor a menor:`);
+console.table(personasOrdenadas);
+console.log(`Promedio de notas general: ${promedioNotas}`);
 
-for(const nota of notasPersonas){
-    sumaNotas+=nota
-}
-
-console.log(notasPersonas)
-console.log(sumaNotas)
-
-rl.close()
+rl.close();
